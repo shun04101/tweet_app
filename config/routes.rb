f3d3a29b799ffd62cfa6b36cea63c75f83485ed2
shun04101 <80952330+shun04101@users.noreply.github.com>
2022-02-27 14:54:49 +0000

    [Add]投稿一覧/投稿詳細/新規投稿

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
-  # get "home/top" => "home#top"
+  get 'posts/index' => "posts#index"
+  get "posts/new" => "posts#new"
+  post "posts/create" => "posts#create"
+  get "posts/:id" => "posts#show"
+
   get "/" => "home#top"
   get "about" => "home#about"
-  
+
 end
