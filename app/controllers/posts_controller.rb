564926442a644d72504b647bf60ebcd142e34e66
shun04101 <80952330+shun04101@users.noreply.github.com>
2022-02-28 13:53:06 +0000

    [Add]投稿編集/投稿削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,4 +18,21 @@ class PostsController < ApplicationController
     redirect_to("/posts/index")
   end
 
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+    redirect_to("/posts/index")
+  end
+  
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to("/posts/index")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get "posts/new" => "posts#new"
   post "posts/create" => "posts#create"
   get "posts/:id" => "posts#show"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
 
   get "/" => "home#top"
   get "about" => "home#about"
