e01d1ab68d4232cc21205a8745783172e4c07452
shun04101 <80952330+shun04101@users.noreply.github.com>
2022-02-28 14:39:31 +0000

    [Add]バリデーション/エラーメッセージ

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,13 +9,18 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
 
   end
 
   def create
     @post = Post.new(content: params[:content])
-    @post.save
-    redirect_to("/posts/index")
+    if @post.save
+      flash[:notice] = "投稿を作成しました"
+      redirect_to("/posts/index")
+    else
+      render("posts/new")
+    end
   end
 
   def edit
@@ -25,13 +30,18 @@ class PostsController < ApplicationController
   def update
     @post = Post.find_by(id: params[:id])
     @post.content = params[:content]
-    @post.save
-    redirect_to("/posts/index")
+    if @post.save
+      flash[:notice] = "投稿を編集しました"
+      redirect_to("/posts/index")
+    else
+      render("posts/edit")
+    end
   end
-  
+
   def destroy
     @post = Post.find_by(id: params[:id])
     @post.destroy
+    flash[:notice] = "投稿を削除"
     redirect_to("/posts/index")
   end
 
