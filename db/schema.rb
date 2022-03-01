3611eb1a20182d09041ff3b2e8c14f34a12bcf3f
shun04101 <80952330+shun04101@users.noreply.github.com>
2022-03-01 06:55:03 +0000

    [Add]ユーザー画像登録/編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_28_145221) do
+ActiveRecord::Schema.define(version: 2022_03_01_051853) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_02_28_145221) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
   end
 
 end
