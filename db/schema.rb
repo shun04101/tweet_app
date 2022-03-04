d46dfddf17123fca5e219b92e279ac0d4eab036c
shun04101 <80952330+shun04101@users.noreply.github.com>
2022-03-04 13:30:24 +0000

    [Add]パスワードのハッシュ化(Gem: bcrypt)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_04_114954) do
+ActiveRecord::Schema.define(version: 2022_03_04_124743) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2022_03_04_114954) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_name"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
